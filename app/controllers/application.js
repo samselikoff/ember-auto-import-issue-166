d2c6f6e7b4b072d1b2f6ffeaa33bb901b98065e5
Sam Selikoff <sam.selikoff@gmail.com>
2018-12-09 17:09:18 -0500

    Bug

---
+++ app/controllers/application.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+
+// Uncomment
+// import dedent from 'dedent';
+
+export default Controller.extend({
+
+  text: `
+    # Hello, world!
+  `
+
+  // Uncomment after starting server to see error. Restart server to see it working.
+  // text: dedent`
+  //   # Hello, world!
+  // `
+
+});
